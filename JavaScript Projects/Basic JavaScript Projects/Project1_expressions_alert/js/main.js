e6f0bf801430ea76dce004651ba44f7065df9e2c
Keaton K <keatonkupillas@yahoo.com>
2023-05-25 15:36:50 -0700

    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT

---
+++ JavaScript Projects/Basic JavaScript Projects/Project1_expressions_alert/js/main.js
@@ -0,0 +1,7 @@
+window.alert("Basic JS");       //made an alert and assigned a string
+var sent1="This is the beginning of the string",sent2=" and this is the end of the string";     //created two variables and assigned them both a string value
+
+function My_First_Function() {
+    var str="This is the button text!";
+    document.getElementById("Button_Text").innerHTML=str;
+}
